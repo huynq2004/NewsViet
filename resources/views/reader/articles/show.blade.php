a9f14fa8f7276d78519b41a3d67febd5e25fc38e
ngoccn <ngochucngoc@gmail.com>
2025-01-03 10:05:38 +0700

    view hien thi chi tiet bai viet

--- resources/views/reader/articles/show.blade.php
+++ resources/views/reader/articles/show.blade.php
@@ -1,10 +1,7 @@
 @extends('layouts.reader')
 
 @section('content')
-    <div class="container mt-4">
-        <h1>{{ $article->title }}</h1>
-        <p><strong>Danh mục:</strong> {{ $article->category->name }}</p>
-        <p>{{ $article->content }}</p>
-        <p><strong>Ngày đăng:</strong> {{ $article->created_at->format('d/m/Y') }}</p>
-    </div>
+    <h2>{{ $article->title }}</h2>
+    <p>{{ $article->content }}</p>
 @endsection
+
