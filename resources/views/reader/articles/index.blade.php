c58e5c3d18893218281cd0311b461d4a9eb023ce
ngoccn <ngochucngoc@gmail.com>
2025-01-03 10:05:24 +0700

    view hien thi reader

--- resources/views/reader/articles/index.blade.php
+++ resources/views/reader/articles/index.blade.php
@@ -1,25 +1,10 @@
-@extends('layouts.reader')
+@extends('reader.layout')
 
 @section('content')
-    <div class="container mt-4">
-        <h1>Các bài viết</h1>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Tiêu đề</th>
-                    <th>Danh mục</th>
-                    <th>Ngày đăng</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($articles as $article)
-                    <tr>
-                        <td><a href="{{ route('reader.articles.show', $article->id) }}">{{ $article->title }}</a></td>
-                        <td>{{ $article->category->name }}</td>
-                        <td>{{ $article->created_at->format('d/m/Y') }}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
+    <h2>Danh sách bài viết</h2>
+    <ul>
+        @foreach($articles as $article)
+            <li><a href="{{ route('reader.articles.show', $article->id) }}">{{ $article->title }}</a></li>
+        @endforeach
+    </ul>
 @endsection
