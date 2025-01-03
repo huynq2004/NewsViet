75657eea1662b06be2d028ad1e9d9e317fb05baf
ngoccn <ngochucngoc@gmail.com>
2025-01-03 10:06:40 +0700

    route reader

--- routes/reader/articles.php
+++ routes/reader/articles.php
@@ -1,10 +1,10 @@
 <?php
-// routes/reader/articles.php
 
 use App\Http\Controllers\ArticleController;
 
 Route::prefix('articles')->group(function () {
+    // Hiển thị danh sách bài viết cho người đọc
     Route::get('/', [ArticleController::class, 'index'])->name('reader.articles.index');
+    // Hiển thị chi tiết bài viết
     Route::get('/{id}', [ArticleController::class, 'show'])->name('reader.articles.show');
 });
-
